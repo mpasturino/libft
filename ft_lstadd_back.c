27e8cbb5544c3da3deae08519637c0987d4a635f
Marco Pasturino <mpasturi@c1r7p1.42sp.org.br>
2020-02-29 19:06:22 -0300

    add front and back added

---
+++ ft_lstadd_back.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpasturi <mpasturi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/29 18:43:16 by mpasturi          #+#    #+#             */
+/*   Updated: 2020/02/29 19:00:15 by mpasturi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*tmp;
+
+	if (*lst != NULL)
+	{
+		tmp = ft_lstlast(*lst);
+		tmp->next = new;
+	}
+	else
+		*lst = new;
+}
