5b86da551e78bb429cd58f8449469e487f504184
Marco Pasturino <mpasturi@c1r7p1.42sp.org.br>
2020-02-29 18:20:12 -0300

    ft_lstnew completo

---
+++ ft_lstnew.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpasturi <mpasturi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/29 18:06:00 by mpasturi          #+#    #+#             */
+/*   Updated: 2020/02/29 18:19:07 by mpasturi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*tmp;
+
+	tmp = malloc(sizeof(t_list));
+	if (tmp != 0)
+	{
+		tmp->content = content;
+		tmp->next = NULL;
+	}
+	return (tmp);
+}
