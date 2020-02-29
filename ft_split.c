e11b6d5db6fb91c701aac4d43d5dca3e3c6c1785
Marco Pasturino <mpasturi@c1r7p1.42sp.org.br>
2020-02-29 16:47:49 -0300

    Part 2 completa

--- ft_split.c
+++ ft_split.c
@@ -6,17 +6,81 @@
 /*   By: mpasturi <mpasturi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/16 17:13:42 by mpasturi          #+#    #+#             */
-/*   Updated: 2020/02/16 17:39:24 by mpasturi         ###   ########.fr       */
+/*   Updated: 2020/02/29 16:27:57 by mpasturi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+static size_t	ft_countarray(char const *s, char c)
 {
-	char	*str;
-	int		len;
+	size_t	i;
+	size_t	count;
 
-	len = ft_strlen(s);
-	
+	i = 0;
+	count = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			if (i > 0 && s[i - 1] != c)
+				count++;
+		}
+		i++;
+	}
+	return (count + 1);
+}
+
+static size_t	ft_countchar(char const *s, char c, size_t i)
+{
+	size_t	j;
+
+	j = 0;
+	while (s[i + j] != c && (i + j) < ((size_t)ft_strlen(s)))
+		j++;
+	return (j);
+}
+
+static char		**ft_fillmatrix(char const *s, char c, char **matrix, size_t r)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (i < ((size_t)ft_strlen(s)))
+	{
+		if (s[i] == c)
+			i++;
+		else
+		{
+			j = ft_countchar(s, c, i);
+			matrix[r] = (char*)malloc((j + 1) * sizeof(char));
+			j = 0;
+			while (s[i] != c && i < ((size_t)ft_strlen(s)))
+			{
+				matrix[r][j] = s[i];
+				i++;
+				j++;
+			}
+			matrix[r][j] = '\0';
+			r++;
+		}
+	}
+	matrix[r] = NULL;
+	return (matrix);
+}
+
+char			**ft_split(char const *s, char c)
+{
+	char	**matrix;
+	size_t	r;
+
+	if (s == 0)
+		return (0);
+	matrix = (char**)malloc((ft_countarray(s, c) + 1) * sizeof(char*));
+	r = 0;
+	if (matrix == 0)
+		return (0);
+	matrix = ft_fillmatrix(s, c, matrix, r);
+	return (matrix);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mpasturi <mpasturi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/26 17:56:39 by mpasturi          #+#    #+#             */
-/*   Updated: 2020/02/16 17:13:32 by mpasturi         ###   ########.fr       */
+/*   Updated: 2020/02/29 16:46:42 by mpasturi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,11 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-
-/* PART 1 */
+typedef struct		s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
@@ -44,7 +47,6 @@ int		ft_tolower(int c);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
 
-/* PART2*/
 char	*ft_itoa(int n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
@@ -55,4 +57,14 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
